71566f041af17dd18b73a094c98c4bbe011c95ae
taoyu <1255803549@qq.com>
2019-12-04 21:11:00 +0800

    spy

---
+++ spy/spy.c
@@ -0,0 +1,90 @@
+#include "stdafx.h"
+
+HMODULE G_HINST_DLL = NULL;
+
+HHOOK G_HK = NULL;
+
+HWND G_DST_HWND = NULL;
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+    switch (fdwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        G_HINST_DLL = hinstDLL;
+        DisableThreadLibraryCalls(hinstDLL);
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+/**
+ * 窗口回调函数
+ *
+ * @param hWnd 窗口句柄
+ * @return
+ */
+LRESULT CALLBACK CallWndProc(int code, WPARAM wParam, LPARAM lParam)
+{
+    CWPSTRUCT *cs = (CWPSTRUCT *)lParam;
+
+    if (code >= 0 && cs)
+    {
+        if (cs->hwnd == G_DST_HWND && cs->message == WM_NULL &&
+            cs->wParam == 0x19820820 && cs->lParam == 0x19831014)
+        {
+            //boost::shared_ptr<std::string> str(new std::string);
+            std::string str;
+            g_bOK = DoSpyIt(G_DST_HWND, str);
+            if (g_bOK)
+            {
+                strncpy_s(g_hwndinfo, NUM_WNDINFO, str.c_str(), sizeof(g_hwndinfo) - 1);
+                g_hwndinfo[sizeof(g_hwndinfo) - 1] = 0;
+            }
+            return g_bOK;
+            //return CallNextHookEx(g_hk,code,wParam,lParam);
+        }
+    }
+    return CallNextHookEx(G_HK, code, wParam, lParam);
+}
+
+/**
+ * 安装钩子
+ *
+ * @param hWnd 窗口句柄
+ * @return
+ */
+BOOL InstallHook(HWND hWnd)
+{
+    DWORD pid, tid;
+    HMODULE hinstDLL = G_HINST_DLL;
+
+    if (G_HK == NULL && IsWindow(hWnd) &&
+        MKTRUE(tid = GetWindowThreadProcessId(hWnd, &pid)) &&
+        pid != GetCurrentProcessId() && hinstDLL &&
+        (G_HK = SetWindowsHookEx(WH_CALLWNDPROC, CallWndProc, hinstDLL, tid)) != NULL)
+    {
+        G_DST_HWND = hWnd;
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+/**
+ * 卸载钩子
+ * @return
+ */
+VOID UninstallHook()
+{
+    G_DST_HWND = NULL;
+    if (G_HK)
+    {
+        UnhookWindowsHookEx(G_HK);
+        G_HK = NULL;
+    }
+}
